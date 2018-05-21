5ff28cd999cdc2629aca0dacdf4cfc95a23184e0
Sam Mulube <sam@thingful.net>
2018-05-21 15:13:53 +0100

    Tweak migrations again just a tiny wee bit
    Alter indexes to be independent

--- pkg/migrations/sql/20180519220506_add_events_table.up.sql
+++ pkg/migrations/sql/20180519220506_add_events_table.up.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS events (
   id SERIAL PRIMARY KEY,
   public_key TEXT NOT NULL,
-  user_id TEXT NOT NULL,
+  user_uid TEXT NOT NULL,
   recorded_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-  data JSONB
+  data BYTEA
 );
 
-CREATE INDEX IF NOT EXISTS events_public_key_index
+CREATE INDEX IF NOT EXISTS events_public_key_idx
   ON events (public_key);
 
-CREATE INDEX IF NOT EXISTS events_user_id_index
-  ON events (user_id);
-
-CREATE INDEX IF NOT EXISTS events_recorded_at
+CREATE INDEX IF NOT EXISTS events_recorded_at_idx
   ON events (recorded_at);
+
+CREATE INDEX IF NOT EXISTS events_user_id_idx
+  ON events (user_uid);
