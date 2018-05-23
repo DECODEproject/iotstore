37eb9d9feee42d7d4dd8f8319bd8d647455b0e8e
Sam Mulube <sam@thingful.net>
2018-05-23 11:02:23 +0100

    add dumb test to show starting/stopping server

--- pkg/server/server_test.go
+++ pkg/server/server_test.go
@@ -3,9 +3,13 @@ package server_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
+	"time"
 
+	kitlog "github.com/go-kit/kit/log"
 	"github.com/stretchr/testify/assert"
+
 	"github.com/thingful/iotstore/pkg/server"
 )
 
@@ -19,4 +23,18 @@ func TestPulseHandler(t *testing.T) {
 	handler.ServeHTTP(rr, req)
 
 	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func TestStartStop(t *testing.T) {
+	logger := kitlog.NewNopLogger()
+	s := server.NewServer("127.0.0.1:0", os.Getenv("IOTSTORE_DATABASE_URL"), logger)
+
+	go func() {
+		s.Start()
+	}()
+
+	time.Sleep(time.Second * 1)
+
+	err := s.Stop()
+	assert.Nil(t, err)
 }
