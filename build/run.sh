915cd087d9968404316eb0c934ec5084ca6a4640
Sam Mulube <sam@thingful.net>
2018-05-25 10:39:20 +0100

    Switch to shared script for entry function that creates the DB

--- build/run.sh
+++ build/run.sh
@@ -9,10 +9,6 @@ if set -o | grep -q "pipefail"; then
   set -o pipefail
 fi
 
-DBNAME=$(basename "$IOTSTORE_DATABASE_URL")
-if ! psql -tc "SELECT 1" "$IOTSTORE_DATABASE_URL" >/dev/null 2>&1; then
-  echo "Creating database $DBNAME"
-  psql -c "CREATE DATABASE $DBNAME" postgres >/dev/null 2>&1;
-fi
+source ./build/shared.sh
 
 "$@"
